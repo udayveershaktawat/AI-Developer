ea2980af7c80afc56af62e48fc25f0aed2eeb13c
udayveershaktawat <udayveershaktawat@gmail.com>
2026-01-04 23:42:10 +0530

    user model created

---
+++ backend/models/user.model.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false,
+  },
+});
+
+userSchema.statics.hashPassword = async function (password) {
+  return await bcrypt.hash(password, 10);
+};
+
+userSchema.methods.isValidPassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+userSchema.methods.generateJWT = function () {
+  return jwt.sign({ email: this.email }, process.env.JWT_SECRET);
+};
+
+const User = mongoose.model("user", userSchema);
+
+export default User;
