3634123b748e9582c4cf4e92a277c02c6d5d82aa
udayveershaktawat <udayveershaktawat@gmail.com>
2026-01-05 22:00:07 +0530

    upadated services

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,1 +1,28 @@
 import userModel from "../models/user.model.js";
+import userService from "../services/user.service.js";
+import {validationResult} from "express-validator"
+
+
+
+export const createUserController = async(req,res)=>{
+    const errors = validationResult(req);
+
+
+    if(!errors.isEmpty()){
+        return res.status(400).json({
+            errors:errors.array(
+
+            )
+        })
+    }
+
+
+    try{
+        const user = await userService.createUser(req.body)
+
+    }
+    catch(error){
+
+    }
+
+}
