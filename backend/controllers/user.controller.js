6ba59f4e5fe2573a09bff91f6a02d8224e3af890
udayveershaktawat <udayveershaktawat@gmail.com>
2026-01-06 23:38:54 +0530

    login created

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -19,3 +19,45 @@ export const createUserController = async (req, res) => {
     res.status(400).send(error.message);
   }
 };
+
+export const loginController = async(req,res)=>{
+  const errors = validationResult(req);
+
+   if (!errors.isEmpty()) {
+    return res.status(400).json({
+      errors: errors.array(),
+    });
+  }
+
+  try{
+
+    const {email,password}= req.body;
+    const user = await userModel.findOne({email});
+
+    if(!user){
+      return res.status(401).json({
+        errors:"invalid credentails"
+      })
+    }
+
+    const isMatch = await user.isValidPassword(password);
+
+    if(!isMatch){
+      return res.status(401).json({
+        errors: 'invalis credentials'
+    })
+    }
+
+    const token = await user.generateJWT();
+
+    res.status(200).json({
+      user,token
+    })
+
+  }
+  catch(error){
+
+     res.status(400).send(error.message);
+  }
+
+}
