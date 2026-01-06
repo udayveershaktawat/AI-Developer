296747a31f62c8b1691f47e936f292da1c97406b
udayveershaktawat <udayveershaktawat@gmail.com>
2026-01-06 23:26:37 +0530

    login route created

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,30 +1,24 @@
-import {Router} from "express";
-import * as UserController  from "../controllers/user.controller.js";
-import {body} from "express-validator"
+import { Router } from "express";
+import * as UserController from "../controllers/user.controller.js";
+import { body } from "express-validator";
 
 const router = Router();
 
-
-
-
-
-
-router.post("/register",
-    body("email").isEmail().withMessage("email must be a email address"),
-    body("password").isLength({min:3}).withMessage("password must be at least 3"),
-    UserController.createUserController)
-
-
-
-
-
-
-
-
-
-
-
-
-
+router.post(
+  "/register",
+  body("email").isEmail().withMessage("email must be a email address"),
+  body("password")
+    .isLength({ min: 3 })
+    .withMessage("password must be at least 3"),
+  UserController.createUserController
+);
+router.post(
+  "/login",
+  body("email").isEmail().withMessage("email must be a email address"),
+  body("password")
+    .isLength({ min: 3 })
+    .withMessage("password must be at least 3"),
+  UserController.loginController
+);
 
 export default router;
