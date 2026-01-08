e7590b8684ee5a64095cfa90851370868ca7fffc
udayveershaktawat <udayveershaktawat@gmail.com>
2026-01-08 23:27:51 +0530

    middleware created

--- backend/middleware/auth.middleware.js
+++ backend/middleware/auth.middleware.js
@@ -6,8 +6,18 @@ export const authUser = async(req,res,next)=>{
 
         const token = req.cookie.token || req.headers.authorization.split(" ")[1];
 
+        if(!token){
+            return res.status(401).send({error:"unauthorized User"})
+        }
+
+
+        const decode = jwt.verify(token,process.env.JWT_SECRET);
+        req.user = decode;
+        next();
+
     }
     catch(error){
+        res.status(401).json({error:"unauthorized User"})
 
     }
 }
