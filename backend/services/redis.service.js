033107161df019f187ac643c21a4008c654efe5f
udayveershaktawat <udayveershaktawat@gmail.com>
2026-01-09 23:22:58 +0530

    redis service completed

--- backend/services/redis.service.js
+++ backend/services/redis.service.js
@@ -4,4 +4,11 @@ const redisClient = new Redis({
     host:process.env.REDIS_HOST,
     port:process.env.REDIS_PORT,
     password:process.env.REDIS_PASSWORD
+});
+
+
+redisClient.on("connect",()=>{
+    console.log("redis connect")
 })
+
+export default  redisClient;
