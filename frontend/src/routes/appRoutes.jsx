dccedb238fe5e7cfbc38a886b975833dec0b6463
udayveershaktawat <udayveershaktawat@gmail.com>
2026-01-11 23:42:04 +0530

    correct all error

--- frontend/src/routes/appRoutes.jsx
+++ frontend/src/routes/appRoutes.jsx
@@ -2,14 +2,15 @@ import React from 'react'
 import { Route,BrowserRouter,Routes } from 'react-router-dom'
 import login from '../screens/login'
 import register from '../screens/register'
+import home from "../screens/home"
 
 const appRoutes = () => {
   return (
    <BrowserRouter>
    <Routes>
-    <Route path='/'  element={<div>Home</div>}/>
-    <Route path='/login' element={login}/>
-    <Route path='/register' element={register}/>
+    <Route path='/'  element={home()}/>
+    <Route path='/login' element={login()}/>
+    <Route path='/register' element={register()}/>
    
 
    </Routes>
--- frontend/src/screens/register.jsx
+++ frontend/src/screens/register.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const register = () => {
   return (
-    <div>
+     <div className='min-h-screen flex items-center justify-center bg-gray-900'>
+      <div className='bg-gray-800 p-8 rounded-lg shadow-lg w-full max-w-md'>
+        <h2>Login</h2>
+
+      </div>
       
     </div>
   )
