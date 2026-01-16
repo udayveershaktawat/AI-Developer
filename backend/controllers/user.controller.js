8b68c5fc567dd4d6da39c8cf9b68171fd831613d
udayveershaktawat <udayveershaktawat@gmail.com>
2026-01-16 23:39:17 +0530

    controller delete user._doc.password added

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -18,7 +18,8 @@ export const createUserController = async(req,res) => {
   try {
     
     const user = await userService.createUser(req.body);
-    const token = await user.generateJWT()
+    const token = await user.generateJWT();
+    delete user._doc.password
     res.status(201).json({user,token})
   } catch (error) {
     res.status(400).send(error.message);
@@ -56,6 +57,7 @@ export const loginController = async(req,res)=>{
     }
 
     const token = await user.generateJWT();
+     delete user._doc.password
 
     res.status(200).json({
       user,token
