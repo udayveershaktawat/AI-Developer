e64ed922e4eb1a5d800367a4af5d8bf38f151c84
udayveershaktawat <udayveershaktawat@gmail.com>
2026-01-16 23:47:38 +0530

    usecontext

---
+++ frontend/src/context/user.context.jsx
@@ -0,0 +1,19 @@
+import { createContext,useState , useContext } from "react";
+
+const userContext = createContext();
+
+
+export const UserProvider = ({children})=>{
+    const [user, setUser] = useState(null);
+
+    return (
+        <userContext.Provider value={{user,setUser}}>
+            {children}
+        </userContext.Provider>
+    )
+
+};
+
+export const useUser = ()=>{
+    return useContext(userContext)
+}
