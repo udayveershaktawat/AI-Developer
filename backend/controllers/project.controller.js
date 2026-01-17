a18c8575a55d197bdb59fbca0d5a014627a2309a
udayveershaktawat <udayveershaktawat@gmail.com>
2026-01-17 23:59:55 +0530

    u

--- backend/controllers/project.controller.js
+++ backend/controllers/project.controller.js
@@ -1,1 +1,20 @@
-import porjectModel from "../models/project.model.js"
+import porjectModel from "../models/project.model.js";
+import projectService from "../services/project.service.js"
+import {validationResult} from "express-validator"
+
+export const createProjectController = async(req,res)=>{
+
+
+    const errors = validationResult(req)
+
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors:errors.array()})
+    }
+
+    const {name}= req.body;
+    const userId = req.user._id;
+
+
+
+    
+}
