e93ea907cc2c1a58713d9430ef88db96a1d69ced
udayveershaktawat <udayveershaktawat@gmail.com>
2026-01-17 23:52:22 +0530

    project service created

---
+++ backend/services/project.service.js
@@ -0,0 +1,23 @@
+import projectModel from "../models/project.model.js";
+
+export const createProject = async({name,userId})=>{
+    if(!name){
+        throw new Error("name is required")
+    }
+    if(!userId){
+        throw new Error("User is required")
+    }
+
+    const project = await projectModel.create({
+        name,
+        users:[userId]
+    })
+
+
+    return project
+
+
+
+
+
+}
