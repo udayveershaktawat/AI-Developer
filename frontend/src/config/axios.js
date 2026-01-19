071f594e309dc90dc5828274114bfdb56ec4a9a0
udayveershaktawat <udayveershaktawat@gmail.com>
2026-01-19 23:31:10 +0530

    axios changes

--- frontend/src/config/axios.js
+++ frontend/src/config/axios.js
@@ -2,6 +2,9 @@ import axios from "axios";
 
 const axiosIntance = axios.create({
 baseURL:import.meta.env.VITE_API_URL,
+headers:{
+    "Authorization":`Bearer ${localStorage.getItem("token")}`
+}
 })
 
 
