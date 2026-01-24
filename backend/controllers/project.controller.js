a1e01614c412d3dd75e6e9efd89128cf63008e0d
udayveershaktawat <udayveershaktawat@gmail.com>
2026-01-24 23:46:47 +0530

    add user project controller created

--- backend/controllers/project.controller.js
+++ backend/controllers/project.controller.js
@@ -53,6 +53,19 @@ export const addUserToProject = async(req,res)=>{
 
     const {projectId,users}=req.body
 
+    const loggedInUser = await userModel.findOne({
+      email:req.user.email
+    })
+    const project  = await projectService.addUsersToProject({
+      projectId,
+      users,
+      userId:loggedInUser._id
+    })
+    
+    return res.status(200).json({
+      project
+    })
+
   }catch(error){
     console.log(error)
     res.status(400).json({error:error.message})
