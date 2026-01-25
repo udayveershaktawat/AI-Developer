c728b6e3d8e825ad9ea300699485191d455e011f
udayveershaktawat <udayveershaktawat@gmail.com>
2026-01-25 23:55:45 +0530

    getprojectby id creating route

--- backend/routes/project.routes.js
+++ backend/routes/project.routes.js
@@ -26,6 +26,11 @@ router.put("/add-user",
     projectController.addUserToProject
 )
 
+router.get("/get-project/:projectId",
+    authMiddleWare.authUser,
+    projectController.getProjectById
+)
+
 
 
 
