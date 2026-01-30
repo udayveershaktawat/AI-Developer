442ba12eddafa660608c6733ec14631c54835b3d
udayveershaktawat <udayveershaktawat@gmail.com>
2026-01-30 23:37:30 +0530

    socket io integrated with frontend

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import http from "http";
 import app from "./app.js";
 import "dotenv/config";
 import {Server} from "socket.io"
+import jwt from "jsonwebtoken"
 
 
 const PORT =  process.env.PORT || 4000
@@ -11,6 +12,35 @@ const PORT =  process.env.PORT || 4000
 const server = http.createServer(app);
 
 const io = new Server(server);
+
+io.use((socket,next)=>{
+    try{
+        const token = socket.handshake.auth?.token || socket.handshake.headers.authorization?.split(" ")[1]
+
+        if(!token){
+            return next(new Error("authorication error"))
+
+        }
+        const decoded = jwt.verify(token,process.env.JWT_SECRET)
+        console.log(decoded)
+
+        if(!decoded){
+            return next(new Error("authorication error"))
+        }
+        socket.user = decoded;
+
+        next();
+
+    }
+    catch(error){
+        next(error)
+    }
+})
+
+
+
+
+
 
 io.on("connection", socket=>{
     console.log("a user connected")
