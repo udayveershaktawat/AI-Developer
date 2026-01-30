9512d3d231aa31e54b4b920ac033b3d18cd31a72
udayveershaktawat <udayveershaktawat@gmail.com>
2026-01-30 23:57:43 +0530

    server socket io created completely

--- frontend/src/config/socket.js
+++ frontend/src/config/socket.js
@@ -1,26 +1,23 @@
-import socket from "socket.io-client"
+import socket from "socket.io-client";
 
+let socketInstance = null;
 
-let socketInstance = null
+export const initializeSocket = () => {
+  socketInstance = socket(import.meta.env.VITE_API_URL, {
+    auth: {
+      token: localStorage.getItem("token"),
+    },
+    query: {
+      projectId,
+    },
+  });
+  return socketInstance;
+};
 
-export const initializeSocket = ()=>{
+export const receiveMessage = (eventName, cb) => {
+  socketInstance.on(eventName, cb);
+};
 
-    socketInstance = socket(import.meta.env.VITE_API_URL,{
-        auth:{
-            token:localStorage.getItem("token")
-        },
-        query:{
-            projectId
-        }
-    });
-    return socketInstance;
-
-}
-
-export const receiveMessage = (eventName,cb)=>{
-    socketInstance.on(eventName,cb);
-}
-
-export const sendMessage = (eventName,data)=>{
-    socketInstance.emit(eventName,data)
-}
+export const sendMessage = (eventName, data) => {
+  socketInstance.emit(eventName, data);
+};
