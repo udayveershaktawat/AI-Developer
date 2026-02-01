69578d460162c821a9cd4ad269bace5ecd632fce
udayveershaktawat <udayveershaktawat@gmail.com>
2026-02-02 00:24:07 +0530

    resolve env bugs and other tailwind buds

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
-})
+  plugins: [react(), , tailwindcss()],
+  server: {
+    headers: {
+      "Cross-Origin-Embedder-Policy": "require-corp",
+      "Cross-Origin-Opener-Policy": "same-origin",
+    },
+    proxy: {
+      "/cdn": {
+        target: "https://unpkg.com",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/cdn/, ""),
+      },
+    },
+  },
+});
